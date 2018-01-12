95a6aaab4d441f40b05d7b5839aafb75d6d09eec
Rebecca Haliburton <haliburtonre@gmail.com>
2018-01-12 14:25:14 -0800

    Created mongo connection

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -14,12 +16,12 @@ var db = mongodb_conn_module.connect();
 var Post = require("../models/post");
 
 app.get('/posts', (req, res) => {
-  Post.find({}, 'title description', function (error, posts) {
-	  if (error) { console.error(error); }
-	  res.send({
+	Post.find({}, 'title description', function (error, posts) {
+		if (error) { console.error(error); }
+		res.send({
 			posts: posts
 		})
-	}).sort({_id:-1})
+	}).sort({ _id: -1 })
 })
 
 app.post('/add_post', (req, res) => {
@@ -44,11 +46,11 @@ app.post('/add_post', (req, res) => {
 app.put('/posts/:id', (req, res) => {
 	var db = req.db;
 	Post.findById(req.params.id, 'title description', function (error, post) {
-	  if (error) { console.error(error); }
+		if (error) { console.error(error); }
 
-	  post.title = req.body.title
-	  post.description = req.body.description
-	  post.save(function (error) {
+		post.title = req.body.title
+		post.description = req.body.description
+		post.save(function (error) {
 			if (error) {
 				console.log(error)
 			}
@@ -63,7 +65,7 @@ app.delete('/posts/:id', (req, res) => {
 	var db = req.db;
 	Post.remove({
 		_id: req.params.id
-	}, function(err, post){
+	}, function (err, post) {
 		if (err)
 			res.send(err)
 		res.send({
@@ -75,8 +77,8 @@ app.delete('/posts/:id', (req, res) => {
 app.get('/post/:id', (req, res) => {
 	var db = req.db;
 	Post.findById(req.params.id, 'title description', function (error, post) {
-	  if (error) { console.error(error); }
-	  res.send(post)
+		if (error) { console.error(error); }
+		res.send(post)
 	})
 })
 
