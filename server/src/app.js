a5fb701596723988ff3f1062340ca9e96cf2f14b
Rebecca Haliburton <haliburtonre@gmail.com>
2018-01-23 16:25:41 -0800

    Api config

--- server/src/app.js
+++ server/src/app.js
@@ -1,89 +1,85 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const helmet = require('helmet')
-const pino = require('pino')()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const pino = require('pino')();
 
-const app = express()
+const app = express();
+
 app.use(express.static('public'));
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(helmet())
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
 
 const url = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
 
-MongoClient.connect(url, function (err, client) {
+MongoClient.connect(url, (err, client) => {
 	if (err) {
 		throw error;
-		pino.error("Could not connect to the databse");
+		pino.error('Could not connect to the databse');
 	}
 
 	const db = client.db(dbName);
 
-	let shipment = db.collection('Shipments');
-	let helpers = require("./helpers");
+	const shipment = db.collection('Shipments');
+	const helpers = require('./helpers');
 
 	app.get('/shipments', (req, res) => {
-		shipment.find({}).toArray(function (error, shipments) {
+		shipment.find({}).toArray((error, shipments) => {
 			if (error) {
 				throw error;
 				pino.error(error);
 			}
 			res.send(shipments);
 		});
-	})
+	});
 
 	app.post('/shipments', (req, res) => {
 		const item = req.body;
-		let _id = item.id;
+		const _id = item.id;
 
-		shipment.count({ _id }, function (error, results) {
+		shipment.count({ _id }, (error, results) => {
 			if (error) {
-				throw err
+				throw err;
 				pino.error(error);
 			}
 			const item = req.body;
-			let _id = item.id;
+			const _id = item.id;
 			if (results == 0) {
 				if (item.status === 'unshipped') {
-					let itemObj = helpers.buildShipment(item);
+					const itemObj = helpers.buildShipment(item);
 
 					helpers.postShipment(shipment, itemObj);
 					res.send({
 						success: 1,
-						type: 'success'
+						type: 'success',
 					});
 				} else {
-					pino.info("Order Already Shipped");
+					pino.info('Order Already Shipped');
 					res.send({
 						skipped: 1,
-						type: 'skipped'
+						type: 'skipped',
 					});
 				}
+			} else if (item.status !== 'unshipped') {
+				helpers.deleteShipment(shipment, item);
+				res.send({
+					deleted: 1,
+					type: 'deleted',
+				});
 			} else {
-				if (item.status !== 'unshipped') {
-					helpers.deleteShipment(shipment, item);
-					res.send({
-						deleted: 1,
-						type: 'deleted'
-					});
-				} else {
-					pino.info("Shipment Already Exists")
-					res.send({
-						skipped: 1,
-						type: 'skipped'
-					});
-				}
+				pino.info('Shipment Already Exists');
+				res.send({
+					skipped: 1,
+					type: 'skipped',
+				});
 			}
-		})
-
-
+		});
 	});
-	app.listen(process.env.PORT || 8081)
-
+	app.listen(process.env.PORT || 8081);
 });
