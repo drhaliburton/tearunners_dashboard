f897e8e306d3d9bdd3d8829e566be30c87593964
Rebecca Haliburton <haliburtonre@gmail.com>
2018-01-27 02:21:51 -0800

    Loading subscriptions

--- server/src/app.js
+++ server/src/app.js
@@ -44,6 +44,16 @@ MongoClient.connect(url, options, function (err, client) {
 		});
 	})
 
+	app.get('/subscriptions', (req, res) => {
+		subscription.find({}).toArray(function (error, subscriptions) {
+			if (error) {
+				throw error;
+				pino.error(error);
+			}
+			res.send(subscriptions);
+		});
+	})
+
 	let prev = '?adjusted_ordered_at__ge=2018-1-15T00:00:00Z';
 
 	app.get('/api/shipments', (req, res) => {
@@ -68,23 +78,11 @@ MongoClient.connect(url, options, function (err, client) {
 							if (results == 0) {
 								if (item.status === 'unshipped') {
 									let itemObj = helpers.buildShipment(item);
-									let posted = helpers.postShipment(shipment, itemObj);
-									posted.then(result => {
-										return result
-									});
-								} else {
-									pino.info('Skipped: ', item.status);
+									helpers.postShipment(shipment, itemObj);
 								}
 							} else {
 								if (item.status !== 'unshipped') {
-									let deleted = helpers.deleteShipment(shipment, item);
-									deleted.then(result => {
-										return result
-									});
-								} else {
-									pino.info("Order already exists")
-									// let updated = helpers.updateShipment(shipment, item);
-									// updated.then(results)
+									helpers.deleteShipment(shipment, item);
 								}
 							}
 						})
@@ -97,6 +95,7 @@ MongoClient.connect(url, options, function (err, client) {
 				}
 				if (next && !retry) {
 					prev = next;
+					pino.info(next);
 
 					let options = {
 						url: process.env.BASE_URL + 'api/shipments',
@@ -133,10 +132,12 @@ MongoClient.connect(url, options, function (err, client) {
 		}
 	})
 
-
+	let subPrev = "";
 
 	app.get('/api/subscriptions', (req, res) => {
-		let params = req.query.next ? req.query.next : '';
+		let next = false;
+		let retry = false;
+		let params = req.query.next ? req.query.next : "?status__eq=2";
 		let options = {
 			url: 'http://api.cratejoy.com/v1/subscriptions/' + params,
 			headers: {
@@ -144,106 +145,69 @@ MongoClient.connect(url, options, function (err, client) {
 				Authorization: process.env.API_AUTH,
 			},
 		}
-		let next = false;
-		request.get(options, function (error, response, body) {
-			let data = JSON.parse(body);
-			for (var item in data.results) {
-				let body = JSON.stringify(data.results[item])
-				let options = {
-					url: process.env.BASE_URL + 'subscriptions/',
-					body,
-					agentOptions: {
-						ciphers: 'DES-CBC3-SHA'
+		if (params == "?status__eq=2" || req.query.next) {
+			request.get(options, (error, response, body) => {
+				if (response.statusCode === 200) {
+					let data = JSON.parse(body);
+					data.results.map(item => {
+						let _id = item.id;
+						let count = subscription.count({ _id })
+						count.then(results => {
+							if (results == 0) {
+								if (item.status === 'active') {
+									let itemObj = helpers.buildSubscription(item);
+									helpers.postSubscription(subscription, itemObj);
+								}
+							} else {
+								if (item.status !== 'active') {
+									helpers.deleteSubscription(shipment, item);
+								}
+							}
+						})
+					})
+					if (data.next) {
+						next = data.next;
 					}
+				} else {
+					retry = true;
 				}
-				request.post(options, function (error, response, body) {
-					if (error) {
-						console.error(error)
+				if (next && !retry) {
+					subPrev = next;
+					pino.info(next)
+
+					let options = {
+						url: process.env.BASE_URL + 'api/subscriptions',
+						qs: {
+							next: next,
+						}
 					}
-				})
-			}
-			if (data.next) {
-				let options = {
-					url: process.env.BASE_URL + 'api/subscriptions',
-					qs: {
-						next: data.next,
-					},
-					headers: {
-						'accept-encoding': 'gzip, deflate'
-					}
-				}
-				setTimeout(function () {
 					request.get(options, function (error, response, body) {
 						if (error) {
 							pino.error(error);
 						}
-					})
-				}, 50)
-			} else {
-				res.send({
-					success: 1,
-					type: 'success'
-				})
-			}
-		})
+					}).end()
+				} else {
+					pino.info('retry', prev);
+					let options = {
+						url: process.env.BASE_URL + 'api/subscriptions',
+						qs: {
+							next: prev,
+						}
+					}
+					request.get(options, function (error, response, body) {
+						if (error) {
+							pino.error(error);
+						}
+					}).end()
+				}
+			}).end()
+		} else {
+			next = prev;
+			console.log("SKIPPED ERR", prev)
+		}
 	})
 
 
-	app.post('/shipments', (req, res) => {
-
-	});
-
-
-
-	app.post('/subscriptions', (req, res) => {
-
-		var body = '';
-		req.on('data', function (data) { body += data; });
-		req.on('end', function (data) {
-			req.body = JSON.parse(body);
-
-			const item = req.body;
-			let _id = item.id;
-
-			subscription.count({ _id }, function (error, results) {
-				if (error) {
-					pino.error(error);
-				}
-				const item = req.body;
-				let _id = item.id;
-				if (results == 0) {
-					if (item.status === 'active') {
-						let itemObj = helpers.buildSubscription(item);
-						helpers.postSubscription(shipment, itemObj);
-						res.send({
-							success: 1,
-							type: 'success'
-						});
-					} else {
-						pino.info("Order Already Shipped");
-						res.send({
-							skipped: 1,
-							type: 'skipped'
-						});
-					}
-				} else {
-					if (item.status !== 'active') {
-						helpers.deleteSubscription(shipment, item);
-						res.send({
-							deleted: 1,
-							type: 'deleted'
-						});
-					} else {
-						pino.info("Subscription Already Exists")
-						res.send({
-							skipped: 1,
-							type: 'skipped'
-						});
-					}
-				}
-			})
-		});
-	});
 
 	const server = app.listen(process.env.PORT || 8081)
 	server.timeout = 240000;
--- server/src/helpers.js
+++ server/src/helpers.js
@@ -22,35 +22,25 @@ module.exports = {
       _id
     };
   },
-  async postShipment(shipment, item) {
-    let results = await shipment.insertOne(item, function (err, r) {
-      if (err) {
-        pino.error(err);
-        setTimeout(function () { return }, 10000);
-      }
+  postShipment(shipment, item) {
+    return shipment.insertOne(item, function (err, r) {
+      if (err) { pino.error(err); }
       pino.info("Shipment Added")
     })
-    return results;
   },
-  async updateShipment(shipment, item) {
-    let results = await shipment.findOneAndUpdate({ _id: item.id }, item, { maxTimeMS: 240000 }, function (error, results) {
+  updateShipment(shipment, item) {
+    return shipment.findOneAndUpdate({ _id: item.id }, item, { maxTimeMS: 240000 }, function (error, results) {
       if (error) {
         pino.error(error);
-        setTimeout(function () { return }, 10000);
       }
       pino.info("Shipment Updated")
     })
-    return results;
   },
-  async deleteShipment(shipment, item) {
-    let results = shipment.deleteOne({ _id: item.id }, function (error, results) {
-      if (error) {
-        pino.error(error);
-        setTimeout(function () { return }, 10000);
-      }
+  deleteShipment(shipment, item) {
+    return shipment.deleteOne({ _id: item.id }, function (error, results) {
+      if (error) { pino.error(error); }
       pino.info(results.deletedCount + " Shipment Deleted")
     })
-    return results;
   },
   buildSubscription(item) {
     let [_id, autorenew, end_date, start_date, status] = [item.id, item.autorenew, item.end_date, item.start_date, item.status];
@@ -60,19 +50,13 @@ module.exports = {
   },
   postSubscription(subscription, item) {
     return subscription.insertOne(item, function (err, r) {
-      if (err) {
-        pino.error(err);
-        setTimeout(function () { return }, 10000);
-      }
+      if (err) { pino.error(err); }
       // pino.info("Subscriptions Added")
     })
   },
   deleteSubscription(subscription, item) {
     return subscription.deleteOne({ _id: item.id }, function (error, results) {
-      if (error) {
-        pino.error(error);
-        setTimeout(function () { return }, 10000);
-      }
+      if (error) { pino.error(error); }
       // pino.info("Subscription Deleted")
     })
   }
