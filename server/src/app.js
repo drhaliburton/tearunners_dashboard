0b3fd881355536de2771e627954cf594ebffcc48
Rebecca Haliburton <haliburtonre@gmail.com>
2018-06-26 13:52:54 -0700

    End of pagination logic

--- server/src/app.js
+++ server/src/app.js
@@ -36,7 +36,6 @@ MongoClient.connect(url, options, function (err, client) {
 	let datahelpers = require("./datahelpers");
 
 
-
 	app.get('/shipments', (req, res) => {
 
 		shipment.find({}).toArray(function (error, shipments) {
@@ -61,13 +60,11 @@ MongoClient.connect(url, options, function (err, client) {
 	});
 
 
-
-	let prev = '?adjusted_ordered_at__ge=2018-1-15T00:00:00Z';
-	let next = false;
+	let next = '?';
 
 	app.get('/api/shipments/', (req, res) => {
 
-		let params = req.query.next ? req.query.next : next ? next : prev;
+		let params = req.query.next ? req.query.next : next;
 
 		let options = {
 			url: 'http://api.cratejoy.com/v1/shipments/' + params,
@@ -101,7 +98,7 @@ MongoClient.connect(url, options, function (err, client) {
 						let data = JSON.parse(body);
 						resolve(data);
 					}
-				})
+				}).end();
 			})
 
 			Promise.all([cratejoyRequest, shipmentsRequest])
@@ -118,10 +115,10 @@ MongoClient.connect(url, options, function (err, client) {
 								if (item.status !== 'unshipped') {
 									let itemObj = helpers.buildShipment(item);
 									let deletePromise = helpers.deleteShipment(db.collection('Shipments'), item);
-									console.log(deletions);
 									shipmentPromises.push(deletePromise);
 									break;
 								} else {
+									console.log(shipmentsDb[shipment], item);
 									let itemObj = helpers.buildShipment(item);
 									let updatePromise = helpers.updateShipment(db.collection('Shipments'), itemObj);
 									shipmentPromises.push(updatePromise);
@@ -138,99 +135,17 @@ MongoClient.connect(url, options, function (err, client) {
 						}
 					})
 
-					return [shipmentPromises, cratejoyData.next];
-				})
-				.then(data => {
-					let cratejoyNext = data[1];
-					if (cratejoyNext) {
-						next = cratejoyNext;
+					if (cratejoyData.next) {
+						next = cratejoyData.next
+					} else {
+						next = false;
 					}
-					Promise.all(data[0])
-						.then(data => {
 
-							if (next) {
-								let options = {
-									url: process.env.BASE_URL + 'api/shipments/',
-									qs: {
-										next: next,
-									}
-								}
-								request.get(options, function (error, response, body) {
-									if (error) {
-										pino.error(error);
-									}
-								}).end()
-							}
-						})
-				})
-				.catch(err => {
-					pino.error(err);
-				})
-		} else {
-			res.send({ success: true })
-		}
-	})
-
-	let subPrev = "?";
-	let subNext = false;
-
-	app.get('/api/subscriptions', (req, res) => {
-		let params = req.query.next ? req.query.next : subNext ? encodeURIComponent(subNext) : subPrev;
-		let options = {
-			url: 'http://api.cratejoy.com/v1/subscriptions/' + params,
-			headers: {
-				'User-Agent': 'request',
-				Authorization: process.env.API_AUTH,
-			},
-		}
-		pino.info(params);
-		if (params) {
-
-			let subscriptionRequest = new Promise((resolve, reject) => {
-				request.get(options, (error, response, body) => {
-					if (error) {
-						if (response.statusCode !== 502) {
-							throw error;
-						}
-					}
-					if (response.statusCode === 200) {
-						let data = JSON.parse(body);
-						resolve(data);
-					}
-				})
-			})
-
-			subscriptionRequest.then(subscriptionData => {
-				subscriptionData.results.map(item => {
-					let _id = item.id;
-					let count = subscription.count({ _id })
-					count.then(results => {
-						if (results == 0) {
-							if (item.status === 'active') {
-								let itemObj = helpers.buildSubscription(item);
-								helpers.postSubscription(subscription, itemObj);
-							}
-						} else {
-							if (item.status !== 'active') {
-								helpers.deleteSubscription(subscription, item);
-							}
-						}
-					})
-				})
-				if (subscriptionData.next) {
-					subNext = subscriptionData.next;
-					return subscriptionData.next;
-				} else {
-					subPrev = subNext;
-				}
-			})
-				.then(next => {
 					if (next) {
-						let query = decodeURIComponent(next);
 						let options = {
-							url: process.env.BASE_URL + 'api/subscriptions/',
+							url: process.env.BASE_URL + 'api/shipments/',
 							qs: {
-								next: query,
+								next: next,
 							}
 						}
 						request.get(options, function (error, response, body) {
@@ -247,6 +162,192 @@ MongoClient.connect(url, options, function (err, client) {
 			res.send({ success: true })
 		}
 	})
+
+	let subNext = '?';
+
+	//update subscription data
+	app.get('/api/subscriptions', (req, res) => {
+
+		let params = req.query.next ? req.query.next : subNext;
+
+		let options = {
+			url: 'http://api.cratejoy.com/v1/subscriptions/' + params,
+			headers: {
+				Connection: 'keep-alive',
+				'User-Agent': 'request',
+				Authorization: process.env.API_AUTH,
+			},
+		}
+		pino.info(params);
+
+		if (params) {
+			let subscriptionRequest = new Promise((resolve, reject) => {
+				subscription.find({}).toArray(function (error, subscriptions) {
+					if (error) {
+						reject(error);
+						pino.error(error);
+					} else {
+						resolve(subscriptions)
+					}
+				});
+			})
+
+			let cratejoyRequest = new Promise((resolve, reject) => {
+				request.get(options, (error, response, body) => {
+					if (error) {
+						if (response.statusCode !== 502) {
+							reject(error);
+						}
+					}
+					if (response.statusCode === 200) {
+						let data = JSON.parse(body);
+						resolve(data);
+					}
+				}).on('end', function () {
+					console.log('ended');
+				})
+			})
+
+
+			Promise.all([cratejoyRequest, subscriptionRequest])
+				.then(promiseData => {
+					let cratejoyData = promiseData[0];
+					let subscriptionDb = promiseData[1];
+					let subscriptionPromises = [];
+
+					cratejoyData.results.map(item => {
+						let cratejoyId = item.id;
+
+						for (var subscription in subscriptionDb) {
+							if (subscriptionDb[subscription]["_id"] === cratejoyId) {
+								if (item.status !== 'active') {
+									let itemObj = helpers.buildSubscription(item);
+									let deletePromise = helpers.deleteSubscription(db.collection('Subscriptions'), item);
+									subscriptionPromises.push(deletePromise);
+									break;
+								} else {
+									let itemObj = helpers.buildSubscription(item);
+									let updatePromise = helpers.postSubscription(db.collection('Subscriptions'), itemObj);
+									subscriptionPromises.push(updatePromise);
+									break;
+								}
+							} else {
+								if (item.status === 'active') {
+									let itemObj = helpers.buildSubscription(item);
+									let postPromise = helpers.postSubscription(db.collection('Subscriptions'), itemObj);
+									subscriptionPromises.push(postPromise);
+									break;
+								}
+							}
+						}
+					})
+					return [subscriptionPromises, cratejoyData.next];
+				})
+				.then(data => {
+					let cratejoyNext = data[1];
+					if (cratejoyNext) {
+						subNext = cratejoyNext;
+					} else {
+						subNext = false;
+					}
+					Promise.all(data[0])
+						.then(data => {
+							if (subNext) {
+								let options = {
+									url: process.env.BASE_URL + 'api/subscriptions/',
+									qs: {
+										next: subNext,
+									}
+								}
+								request.get(options, function (error, response, body) {
+									if (error) {
+										pino.error(error);
+									}
+								})
+							}
+						})
+				})
+				.catch(err => {
+					pino.error(err);
+				})
+		} else {
+			res.send({ success: true })
+		}
+	})
+
+	// app.get('/api/subscriptions', (req, res) => {
+	// 	let params = req.query.next ? req.query.next : subNext ? encodeURIComponent(subNext) : subPrev;
+	// 	let options = {
+	// 		url: 'http://api.cratejoy.com/v1/subscriptions/' + params,
+	// 		headers: {
+	// 			'User-Agent': 'request',
+	// 			Authorization: process.env.API_AUTH,
+	// 		},
+	// 	}
+	// 	pino.info(params);
+	// 	if (params) {
+
+	// 		let subscriptionRequest = new Promise((resolve, reject) => {
+	// 			request.get(options, (error, response, body) => {
+	// 				if (error) {
+	// 					if (response.statusCode !== 502) {
+	// 						throw error;
+	// 					}
+	// 				}
+	// 				if (response.statusCode === 200) {
+	// 					let data = JSON.parse(body);
+	// 					resolve(data);
+	// 				}
+	// 			})
+	// 		})
+
+	// 		subscriptionRequest.then(subscriptionData => {
+	// 			subscriptionData.results.map(item => {
+	// 				let _id = item.id;
+	// 				let count = subscription.count({ _id })
+	// 				count.then(results => {
+	// 					if (results == 0) {
+	// 						if (item.status === 'active') {
+	// 							let itemObj = helpers.buildSubscription(item);
+	// 							helpers.postSubscription(subscription, itemObj);
+	// 						}
+	// 					} else {
+	// 						if (item.status !== 'active') {
+	// 							helpers.deleteSubscription(subscription, item);
+	// 						}
+	// 					}
+	// 				})
+	// 			})
+	// 			if (subscriptionData.next) {
+	// 				subNext = subscriptionData.next;
+	// 				return subscriptionData.next;
+	// 			} else {
+	// 				subPrev = subNext;
+	// 			}
+	// 		})
+	// 			.then(next => {
+	// 				if (next) {
+	// 					let query = decodeURIComponent(next);
+	// 					let options = {
+	// 						url: process.env.BASE_URL + 'api/subscriptions/',
+	// 						qs: {
+	// 							next: query,
+	// 						}
+	// 					}
+	// 					request.get(options, function (error, response, body) {
+	// 						if (error) {
+	// 							pino.error(error);
+	// 						}
+	// 					}).end()
+	// 				}
+	// 			})
+	// 			.catch(err => {
+	// 				pino.error(err);
+	// 			})
+	// 	} else {
+	// 		res.send({ success: true })
+	// 	}
+	// })
 
 	const server = app.listen(process.env.PORT || 8081)
 	server.timeout = 240000;
